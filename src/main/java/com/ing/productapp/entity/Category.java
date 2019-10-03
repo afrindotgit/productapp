044304c23483f3bd1129ec3ebb826b7e82d08798
User1 <User1@LP-5CD9296CLH.HCLT.CORP.HCL.IN>
2019-10-03 14:36:34 +0530

    Entity Creation

---
+++ src/main/java/com/ing/productapp/entity/Category.java
@@ -0,0 +1,42 @@
+package com.ing.productapp.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long categoryId;
+	@Column(nullable = false)
+	private String categoryName;
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [categoryId=" + categoryId + ", categoryName=" + categoryName + "]";
+	}
+
+}
