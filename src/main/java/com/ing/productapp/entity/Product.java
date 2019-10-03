e00e697cbb3ea209a2c3a7047ab9b6c9938b05f4
User1 <User1@LP-5CD9176M8Z.HCLT.CORP.HCL.IN>
2019-10-03 15:03:03 +0530

    Initial Desgin

---
+++ src/main/java/com/ing/productapp/entity/Product.java
@@ -0,0 +1,76 @@
+package com.ing.productapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long productId;
+	private String productName;
+	private Double price;
+	private Double rating;
+	private String description;
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", price=" + price + ", rating="
+				+ rating + ", description=" + description + ", categoryId=" + categoryId + "]";
+	}
+
+	@ManyToOne
+	private Long categoryId;
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+}
