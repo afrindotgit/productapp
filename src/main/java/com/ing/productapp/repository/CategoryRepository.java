036bde508cb428a545dece60ff77906f0814a4b0
User1 <User1@LP-5CD9176M8Z.HCLT.CORP.HCL.IN>
2019-10-03 19:54:46 +0530

    Changes

--- src/main/java/com/ing/productapp/repository/CategoryRepository.java
+++ src/main/java/com/ing/productapp/repository/CategoryRepository.java
@@ -1,14 +1,5 @@
 package com.ing.productapp.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ing.productapp.dto.CategoryInterfaceResponseDto;
-import com.ing.productapp.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-	List<Category> findAll();
+public interface CategoryRepository {
 
 }
